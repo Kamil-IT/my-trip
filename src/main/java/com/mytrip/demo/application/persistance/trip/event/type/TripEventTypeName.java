9befd8da135c555358e1e455ed61f4d56b0ecbf1
kkwol <kkwolny@vp.pl>
2022-09-29 22:22:49 +0200

    edit trip

--- src/main/java/com/mytrip/demo/application/persistance/trip/event/type/TripEventTypeName.java
+++ src/main/java/com/mytrip/demo/application/persistance/trip/event/type/TripEventTypeName.java
@@ -2,5 +2,18 @@ package com.mytrip.demo.application.persistance.trip.event.type;
 
 public enum TripEventTypeName {
 
-    ACCOMMODATION, TRANSFER, TOUR
+    ACCOMMODATION("Accommodation"),
+    TRANSFER("Transfer"),
+    TOUR("Accommodation");
+
+
+    private final String displayValue;
+
+    TripEventTypeName(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }
