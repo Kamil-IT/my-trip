3ed110cff370299fae21b0ae888fbdf27b808e6e
kkwol <kkwolny@vp.pl>
2022-09-30 11:52:20 +0200

    move packages

--- src/main/java/com/mytrip/demo/application/exception/ResourceNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package com.mytrip.demo.application.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-}
--- src/main/java/com/mytrip/demo/infrastructure/booking/BookingClient.java
+++
@@ -1,6 +0,0 @@
-package com.mytrip.demo.infrastructure.booking;
-
-public class BookingClient {
-
-//    https://rapidapi.com/apidojo/api/booking/pricing
-}
