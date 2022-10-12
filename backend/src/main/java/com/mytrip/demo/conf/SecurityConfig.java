8cedb2c414cc9c49457e61049872a7503c32ff6a
KWolny <kkwolny@vp.pl>
2022-10-12 23:52:27 +0200

    add todo

--- backend/src/main/java/com/mytrip/demo/conf/SecurityConfig.java
+++ backend/src/main/java/com/mytrip/demo/conf/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+// TODO: fix h2 db
 //@Configuration
 //public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //
