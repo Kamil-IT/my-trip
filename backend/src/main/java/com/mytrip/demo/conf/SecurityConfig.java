a0a14568092624b3888b46af400dd75d3a19f877
KWolny <kkwolny@vp.pl>
2022-10-16 11:05:32 +0200

    config h2

--- backend/src/main/java/com/mytrip/demo/conf/SecurityConfig.java
+++ backend/src/main/java/com/mytrip/demo/conf/SecurityConfig.java
@@ -1,19 +1,56 @@
 package com.mytrip.demo.conf;
 
-import org.springframework.context.annotation.Configuration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-// TODO: fix h2 db
-//@Configuration
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity httpSecurity) throws Exception {
-//        httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
-//                .authorizeRequests().antMatchers("/console/**").permitAll();
-//        httpSecurity.csrf().disable();
-//        httpSecurity.headers().frameOptions().disable();
-//        httpSecurity.httpBasic().disable();
-//    }
-//}
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder,
+                                             UserDetailsService userDetailService)
+            throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailService)
+                .passwordEncoder(bCryptPasswordEncoder)
+                .and()
+                .build();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().disable();
+
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/v1/user")
+                .permitAll()
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/console/**")
+                .permitAll()
+                .and()
+                .csrf().disable()
+                .httpBasic();
+
+        return http.build();
+    }
+}
